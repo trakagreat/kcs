0b477eb1bd64d8afedcb4e506db5717d5380f860
trivendra rahi <62616657+trakagreat@users.noreply.github.com>
2022-11-01 07:41:44 -0700

    Delete kcs directory

--- kcs/sugar/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class ClientModel(models.Model):
-    name = models.CharField(max_length=30)
-    number = models.CharField(max_length=30)
